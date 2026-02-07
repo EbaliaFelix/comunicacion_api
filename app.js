9c0949d9906d2f77e54fc9aff3025c223aeeb161
Ebalia Felix <fy.felix1@gmail.com>
2026-02-06 18:40:28 -0700

    Conectando API TMDb

---
+++ app.js
@@ -0,0 +1,39 @@
+const btn = document.getElementById("cargarBtn");
+const contenedor = document.getElementById("contenedor");
+const apiKey = "395f3abc3cfa6bf9cb5cd50c9de0e266";
+
+
+btn.addEventListener("click", obtenerUsuarios);
+
+async function buscarPeliculas() {
+  const url = `https://api.themoviedb.org/3/search/movie?api_key=${apiKey}&query=cultura`;
+
+
+   try {
+    const respuesta = await fetch(url);
+    const data = await respuesta.json();
+    mostrarPeliculas(data.results);
+  } catch (error) {
+    console.log("Error:", error);
+  }
+}
+
+function mostrarPeliculas(peliculas) {
+  const contenedor = document.getElementById("contenedor");
+  contenedor.innerHTML = "";
+
+  peliculas.forEach(pelicula => {
+    const card = document.createElement("div");
+    card.classList.add("card");
+
+    card.innerHTML = `
+      <h3>${pelicula.title}</h3>
+      <p>${pelicula.overview || "Sin descripción"}</p>
+      <img src="https://image.tmdb.org/t/p/w500${pelicula.poster_path}" alt="">
+    `;
+
+    contenedor.appendChild(card);
+  });
+}
+
+buscarPeliculas();
