c1b7bc146e6140e33c3f236e81a56c25ad392479
Ebalia Felix <fy.felix1@gmail.com>
2026-02-20 23:02:07 -0700

    Aplicación simple con fetch y JSON.stringify funcionando

--- app.js
+++ app.js
@@ -1,37 +1,12 @@
-const btn = document.getElementById("cargarBtn");
-const contenedor = document.getElementById("contenedor");
-const apiKey = "395f3abc3cfa6bf9cb5cd50c9de0e266";
+async function consultarAPI() {
+  try {
+    const response = await fetch("https://api.github.com/users/octocat");
+    const data = await response.json();
 
-btn.addEventListener("click", buscarPeliculas);
+    document.getElementById("resultado").textContent =
+      JSON.stringify(data, null, 2);
 
-async function buscarPeliculas() {
-  const url = `https://api.themoviedb.org/3/search/movie?api_key=${apiKey}&query=cultura`;
-
-
-   try {
-    const respuesta = await fetch(url);
-    const data = await respuesta.json();
-    mostrarPeliculas(data.results);
   } catch (error) {
-    console.log("Error:", error);
+    console.error("Error:", error);
   }
 }
-
-function mostrarPeliculas(peliculas) {
-  const contenedor = document.getElementById("contenedor");
-  contenedor.innerHTML = "";
-
-  peliculas.forEach(pelicula => {
-    const card = document.createElement("div");
-    card.classList.add("card");
-
-    card.innerHTML = `
-      <h3>${pelicula.title}</h3>
-      <p>${pelicula.overview || "Sin descripción"}</p>
-      <img src="https://image.tmdb.org/t/p/w500${pelicula.poster_path}" alt="">
-    `;
-
-    contenedor.appendChild(card);
-  });
-}
-
